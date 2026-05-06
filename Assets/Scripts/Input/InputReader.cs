36807c1981463ec1709f869d32a57bd6892495d6
SuperYoshiStar <quickscopesuperyoshistar@gmail.com>
2026-05-06 15:35:24 -0700

    movement package+inputreader setup

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using NewerInput;
+using UnityEngine.InputSystem;
+using System;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Input Reader")]
+public class InputReader : ScriptableObject, RealInput.IPlayerActions
+{
+    public RealInput realInput;
+
+    private void OnEnable()
+    {
+        if (realInput == null)
+        {
+            realInput = new RealInput();
+            realInput.Player.SetCallbacks(this);
+        }
+
+        SetPlayer();
+    }
+
+    private void OnDisable()
+    {
+        DisableAll();
+    }
+
+    public void SetPlayer()
+    {
+        DisableAll();
+        realInput.Player.Enable();
+    }
+
+    //disable all input maps
+    public void DisableAll()
+    {
+        realInput.Player.Disable();
+    }
+
+    public event Action<Vector2> MoveEvent;
+    public event Action<Vector2> LookEvent;
+    public event Action SprintEvent;
+    public event Action InteractEvent;
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            InteractEvent?.Invoke();
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        LookEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            SprintEvent?.Invoke();
+        }
+    }
+}
