ead19894dcbaceea138db113ecb3f235cf25d815
Adira Rana <diamondadira@gmail.com>
2026-05-14 19:21:00 -0700

    changed background image to something slightly more fitting

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,7 +5,7 @@ public class MainMenuScript : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("NewMovement");
+        SceneManager.LoadScene("WorldSketch");
     }
     public void QuitGame()
     {
